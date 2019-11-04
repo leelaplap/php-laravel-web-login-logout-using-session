f5f0b49b7c466278e2e9c8e99d43fc5660698719
leelaplap <letandat27101998@gmail.com>
2019-11-04 16:29:00 +0700

    update commmit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 'LoginController@showLogin')->name('show.login');
+Route::post('/login', 'LoginController@login')->name('user.login');
+Route::get('/blog', 'BlogController@showBlog')->name('show.blog');
+Route::get('/logout', 'LogoutController@logout')->name('user.logout');
